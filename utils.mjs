7cc38094bd3a713145d6fddf65c6f6a3e9e703d5
johackim <contact@johackim.com>
2023-05-13 22:36:46 +0200

    feat: update ulysse

--- utils.mjs
+++ utils.mjs
@@ -11,9 +11,9 @@ const configPath = params.includes('--config') ? params[params.indexOf('--config
 export const config = JSON.parse(await fs.readFileSync(configPath, 'utf8'));
 
 export const blockHosts = async () => {
-    const { hosts } = config;
+    const { blocklist } = config;
 
-    fs.writeFileSync('hosts.txt', hosts.map((host) => `127.0.0.1 ${host} www.${host}`).join('\n'), 'utf8');
+    fs.writeFileSync('/etc/hosts', blocklist.map((host) => `127.0.0.1 ${host} www.${host}`).join('\n'), 'utf8');
 };
 
 export const unBlockHosts = async () => {
